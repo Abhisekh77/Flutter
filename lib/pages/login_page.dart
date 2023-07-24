f14edfba6fbddcdec7de97766a8131d5017d0185
Abhisekh77 <avishekkhanal750@gmail.com>
2023-07-24 20:43:23 +0545

    day4completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_project/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,56 +7,64 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-        color: Colors.black,
-        child: Scaffold(
-            body: Column(
-          children: [
-            Image.asset(
-              "assets/login_image.png",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              "Welcome to Login Page",
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Padding(
-              // padding: const EdgeInsets.all(20.0),
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter Username",
-                      labelText: "Username",
-                    ),
-                  ),
-                  TextFormField(
-                    // obscureText ley password lai hide garxa, bydefault false hunxa
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Enter Password",
-                      labelText: "Password",
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      print("Hello abhi");
-                    },
-                    child: Text("Login"),
-                  )
-                ],
+        child: SingleChildScrollView(
+            child: Column(
+      children: [
+        Image.asset(
+          "assets/login_image.png",
+          fit: BoxFit.cover,
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          "Welcome to Login Page",
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Padding(
+          // padding: const EdgeInsets.all(20.0),
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Enter Username",
+                  labelText: "Username",
+                ),
               ),
-            ),
-          ],
-        )));
+              TextFormField(
+                // obscureText ley password lai hide garxa, bydefault false hunxa
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Enter Password",
+                  labelText: "Password",
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, MyRoutes.homeRoute);
+                },
+                child: Text("Login"),
+                style: TextButton.styleFrom(minimumSize: Size(130, 40)),
+              ),
+              Image.asset(
+                "assets/login_image.png",
+                fit: BoxFit.cover,
+              ),
+              Image.asset(
+                "assets/login_image.png",
+                fit: BoxFit.cover,
+              ),
+            ],
+          ),
+        ),
+      ],
+    )));
   }
 }
